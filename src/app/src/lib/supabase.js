a3509d731c94f2c418ba34c40c3d603c08c9d0fb
enosmoraessantos-svg <enosmoraes.santos@gmail.com>
2026-04-28 15:09:57 -0300

    Delete src/app/src/lib directory

--- src/app/src/lib/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SB_URL = "https://yczhjyxisqrvfxxgipop.supabase.co"; 
-const SB_KEY = "sb_publishable_6SmMDIymguOi59WrVb1QQA_wSykUdUm";
-
-// Inicialização conforme solicitado
-export const supabaseClient = createClient(SB_URL, SB_KEY);
